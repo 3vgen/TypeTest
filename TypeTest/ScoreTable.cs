859b07bfde759c744c4d1d8068e7bff48ab122bc
3vgen <evgeniy.zhenia@gmail.com>
2023-01-19 00:53:48 +0300

    Добавьте файлы проекта.

---
+++ TypeTest/ScoreTable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text;
+
+namespace TypeTest
+{
+    public class ScoreTable
+    {
+        string path = @"";
+        public List<User> users = new List<User>();
+        public ScoreTable(string path)
+        {
+            this.path = path;
+        }
+        public void ReadJson()
+        {
+            StreamReader reader = new StreamReader(path);
+            users = JsonSerializer.Deserialize<List<User>>(reader.ReadToEnd());
+            reader.Close();
+        }
+       
+        public void WriteJson()
+        {
+            //File.WriteAllText(path, string.Empty);
+            string json = JsonSerializer.Serialize(users, typeof(List<User>));
+            StreamWriter writer = new StreamWriter(path);
+            writer.Write(json);
+            writer.Close();
+        }
+        public void AddUser(User user)
+        {
+            users.Add(user);
+        }
+        public void PrintTable()
+        {
+            Console.WriteLine("Таблица рекордов");
+            Console.WriteLine("----------------------------------------------");
+            foreach(var item in users)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ TypeTest/Texts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypeTest
+{
+    class Texts
+    {
+        Random random = new Random();
+        string[] texsts;
+        StreamReader read;
+        public Texts()
+        {
+            read = new StreamReader(@"C:\Users\МБОУ ЦО 2\source\repos\TypeTest\TypeTest\TextSamples.txt");
+            texsts = read.ReadToEnd().Split('#');
+        }
+        public string GetText()
+        {
+            string text;
+            text = texsts[random.Next(texsts.Length)];
+            return text;
+        }
+
+
+    }
+}
